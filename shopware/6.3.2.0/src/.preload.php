a30a12980509e6827a6eb6d3dbf912d5aa76004b
Leon <l.weickert@loewenstark.com>
2020-10-05 16:41:09 +0200

    add 6.3.2.0

---
+++ shopware/6.3.2.0/src/.preload.php
@@ -0,0 +1,5 @@
+<?php
+
+if (file_exists(__DIR__.'/../var/cache/prod_hab4bd0ecf633c9ff5c1262e151066a63/srcShopware_Production_KernelProdContainer.preload.php')) {
+    require __DIR__.'/../var/cache/prod_hab4bd0ecf633c9ff5c1262e151066a63/srcShopware_Production_KernelProdContainer.preload.php';
+}
