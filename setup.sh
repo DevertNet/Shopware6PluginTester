6c7a2e3539349943252267d56747592cc28c201f
Leon <l.weickert@loewenstark.com>
2020-12-10 19:46:29 +0100

    add sw 6.3.4.0

--- setup.sh
+++ setup.sh
@@ -26,7 +26,7 @@ if [ -z "$TESTER_MODE" ]; then
     TESTER_MODE='all'
 fi
 
-SHOPWARE_VERSIONS=(6.3.2.1)
+SHOPWARE_VERSIONS=(6.3.4.0)
 
 # Launch & run Shopware Versions
 for i in "${SHOPWARE_VERSIONS[@]}"
