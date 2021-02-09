421db4e6b7bfa3605ee6368f687848d65b0e6d66
Leon <l.weickert@loewenstark.com>
2021-02-09 20:23:53 +0100

    refactor: add new sw version to sh files

--- setup.sh
+++ setup.sh
@@ -26,7 +26,7 @@ if [ -z "$TESTER_MODE" ]; then
     TESTER_MODE='all'
 fi
 
-SHOPWARE_VERSIONS=(6.3.5.0)
+SHOPWARE_VERSIONS=(6.3.5.1)
 
 # Launch & run Shopware Versions
 for i in "${SHOPWARE_VERSIONS[@]}"
