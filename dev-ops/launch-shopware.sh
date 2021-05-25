7232f67362963e94e59a8257c155d291e14c27c9
Leon Weickert <leon@MacBook-Air-von-Leon.local>
2021-05-25 22:28:30 +0200

    fix: use ddev composer instead of user system composer

--- dev-ops/launch-shopware.sh
+++ dev-ops/launch-shopware.sh
@@ -5,14 +5,14 @@ SW_PATH=$1/shopware/$2;
 PLUGIN_NAME=$3
 
 # Launch Shopware
+cd $SW_PATH && ddev start
 cd $SW_PATH && rm -rf .env
-cd $SW_PATH && composer install
+cd $SW_PATH && ddev composer install
 cd $SW_PATH && cp .env.plugintester .env
 rm -rf $SW_PATH/config/jwt/private.pem
 cp $DIR/dev-ops/launch-shopware/jwt/private.pem $SW_PATH/config/jwt/private.pem
 rm -rf $SW_PATH/config/jwt/public.pem
 cp $DIR/dev-ops/launch-shopware/jwt/public.pem $SW_PATH/config/jwt/public.pem
-cd $SW_PATH && ddev start
 cd $SW_PATH && ddev import-db --src=.ddev/dump.sql
 cd $SW_PATH && ddev exec ./bin/console cache:clear
 cd $SW_PATH && ddev exec ./bin/console assets:install
